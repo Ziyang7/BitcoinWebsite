4a11d2d9c1dc30e6ebef385d7704008d48339361
Ziyang Li <ziyangli103@gmail.com>
2019-12-05 09:49:26 -0600

    ready

---
+++ BitcoinStore/src/app/products.ts
@@ -0,0 +1,49 @@
+export const products = [
+  {
+    name: 'Phone',
+    price: 3,
+    description: 'a large phone with a good screen'
+  },
+  {
+    name: 'Iron Bowl Ticket',
+    price: 2,
+    description: 'a great game (cannot reverse payment)'
+  },
+  {
+    name: 'Leaf',
+    price: 0.0001,
+    description: 'a cheap leaf'
+  },
+    {
+    name: 'The World Might Be Better Off Without College for Everyone',
+    price: 1,
+    description: 'text written by Caplan'
+  },
+    {
+    name: 'Diamond-Water Paradox',
+    price: 1,
+    description: 'text written by Adam Smith'
+  },
+    {
+    name: 'Sales Pitch',
+    price: 1,
+    description: 'text written by Philip K. Dick'
+  },
+    {
+    name: 'Hayek dependence effect',
+    price: 1,
+    description: 'text written by Friedrich Hayek'
+  },
+  {
+    name: 'ENGL-1120-025 (Fall 2019)',
+    price: 1,
+    description: 'a fun class for the whole family'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
